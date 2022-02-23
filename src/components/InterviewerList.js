8fcab6bebdfa719b4cd0ddbf6bdd92cf5d9be9e5
Kagin007 <adamschulte148@hotmail.com>
2022-02-22 20:36:55 -0500

    add componentlist html, css

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,13 @@
+import React from 'react';
+import "./InterviewerListItem.scss"
+
 function InterviewerList(props) {
   return (
-
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
   );
 }
+
+export default InterviewerList;
