972a1b14a13626575dae05c9dfd3927af54f6941
Kagin007 <adamschulte148@hotmail.com>
2022-02-24 21:29:54 -0500

    add helpers for appointment filtering

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,28 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import axios from 'axios';
+
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = [
   {
@@ -63,6 +65,14 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday')
+  const [days, setDays] = useState([])
+
+  useEffect( () => {
+    axios.get('/api/days')
+    .then(response => {
+      setDays([...response.data])
+    })
+  }, [])
 
   return (
     <main className="layout">
